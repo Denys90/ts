b29c0df282fca15a0acb26b2276104e54e503a1e
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-07-10 14:17:49 +0200

    context

--- src/App.tsx
+++ src/App.tsx
@@ -292,48 +292,105 @@ import ReactDOM from "react-dom";
 
 // -------------------------------------------------------------------
 
-interface PrtalProps {
-  children: React.ReactNode;
+// interface PrtalProps {
+//   children: React.ReactNode;
+// }
+
+// class Portal extends Component<PrtalProps> {
+//   private el: HTMLDivElement = document.createElement("div");
+
+//   public componentDidMount(): void {
+//     document.body.appendChild(this.el);
+//   }
+
+//   public componentWillUnmount(): void {
+//     document.body.removeChild(this.el);
+//   }
+
+//   public render(): React.ReactElement<PrtalProps> {
+//     return ReactDOM.createPortal(this.props.children, this.el);
+//   }
+// }
+
+// class MyComponent extends Component<{}, { count: number }> {
+//   state = {
+//     count: 0,
+//   };
+
+//   handleClick = () => {
+//     this.setState(({ count }) => ({
+//       count: ++count,
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <div onClick={this.handleClick}>
+//         <h1>Clicks: {this.state.count}</h1>
+//         <Portal>
+//           <h2>TEST PORTAL</h2>
+//           <button>Click</button>
+//         </Portal>
+//       </div>
+//     );
+//   }
+// }
+// const App: React.FC = () => <MyComponent />;
+// export default App;
+
+// -------------------------------------------------------------------
+
+interface IContext {
+  isAuth: boolean;
+  toggleAuth: () => void;
 }
 
-class Portal extends Component<PrtalProps> {
-  private el: HTMLDivElement = document.createElement("div");
+const AuthContext = React.createContext<IContext>({
+  isAuth: false,
+  toggleAuth: () => {},
+});
 
-  public componentDidMount(): void {
-    document.body.appendChild(this.el);
-  }
+// new syntex of static property
+class Login extends Component {
+  static contextType = AuthContext;
+  context!: React.ContextType<typeof AuthContext>;
 
-  public componentWillUnmount(): void {
-    document.body.removeChild(this.el);
-  }
+  render() {
+    const { toggleAuth, isAuth } = this.context;
 
-  public render(): React.ReactElement<PrtalProps> {
-    return ReactDOM.createPortal(this.props.children, this.el);
+    return <button onClick={toggleAuth}>{!isAuth ? "Login" : "Logout"}</button>;
   }
 }
 
-class MyComponent extends Component<{}, { count: number }> {
-  state = {
-    count: 0,
+// old variant with Consumer
+const Profile: React.FC = (): React.ReactElement => (
+  <AuthContext.Consumer>
+    {({ isAuth }: IContext) => <h1>{!isAuth ? "Please log in" : "You are logged in"}</h1>}
+  </AuthContext.Consumer>
+);
+
+// Root component
+class Context extends Component<{}, { isAuth: boolean }> {
+  readonly state = {
+    isAuth: false,
   };
 
-  handleClick = () => {
-    this.setState(({ count }) => ({
-      count: ++count,
-    }));
+  toggleAuth = () => {
+    this.setState(({ isAuth }) => ({ isAuth: !isAuth }));
   };
 
   render() {
+    const { isAuth } = this.state;
+    const context: IContext = { isAuth, toggleAuth: this.toggleAuth };
+
     return (
-      <div onClick={this.handleClick}>
-        <h1>Clicks: {this.state.count}</h1>
-        <Portal>
-          <h2>TEST PORTAL</h2>
-          <button>Click</button>
-        </Portal>
-      </div>
+      <AuthContext.Provider value={context}>
+        <Login />
+        <Profile />
+      </AuthContext.Provider>
     );
   }
 }
-const App: React.FC = () => <MyComponent />;
+
+const App: React.FC = () => <Context />;
 export default App;
